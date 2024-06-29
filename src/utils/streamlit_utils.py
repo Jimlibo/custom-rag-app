00b9453a1da7572633048c79e01b48a035aa64ae
lymperop <jimlibo13@gmail.com>
2024-06-29 13:42:15 +0300

    initial commit for utility functions related to streamlit contents

---
+++ src/utils/streamlit_utils.py
@@ -0,0 +1,20 @@
+"""
+Created on 29 June 2024
+@authro: Dimitris Lymperopoulos
+Description: A script containing utility functions for streamlit content
+"""
+
+import os
+import streamlit as st
+
+
+def home_page(db_agent):
+    pass
+
+
+def upload_documents(db_agent):
+    pass
+
+
+def query_documents(db_agent):
+    pass
